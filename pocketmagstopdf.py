ff30bc1e00308ed262f856a5753f819f0c06ab8b
Richard Leszczynski <59875350+RichardJRL@users.noreply.github.com>
2022-07-14 01:16:46 +0100

    Now able to download, edit and save high quality "bin" format files.

--- pocketmagstopdf.py
+++ pocketmagstopdf.py
@@ -11,14 +11,16 @@
 Download a pocketmags magazines in PDF format from the HTML5 reader.
 
 Usage:
-    pmdown.py (-h | --help)
-    pmdown.py [options] <pdf> <url>
+    pocketmagstopdf.py (-h | --help)
+    pocketmagstopdf.py [options] <pdf> <url>
 
 Options:
 
     -h, --help                  Print brief usage summary.
     --dpi=DPI                   Set image resolution in dots per inch.
                                 [default: 150]
+    --quality=QUALITY           Set magazine download quality; Choose from extralow, low, mid or high.
+                                [default: mid]
 
     <pdf>                       Save output to this file.
     <url>                       A URL to one image from the magazine.
@@ -46,10 +48,15 @@ from urllib.error import HTTPError
 from urllib.parse import urlparse, urlunparse
 from urllib.request import urlopen
 
+import binascii
 import docopt
+import PIL
+from io import BytesIO
 from PIL import Image
+from PIL import UnidentifiedImageError
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
+
 
 # The pattern of the URL path for a magazine
 # Original Gist pattern
@@ -57,7 +64,10 @@ from reportlab.lib.units import inch
 # One of my updated patterns
 # URL_PATH_PATTERN = re.compile(r'(?P<prefix>/mcmags/[a-f0-9\-]*/[a-f0-9\-]*/extralow/)[0-9]{4}.jpg')
 # One of my updated patterns, confirmed working 8 July 2022
-URL_PATH_PATTERN = re.compile(r'(?P<prefix>^/mcmags/[a-f0-9\-/]*/mid/)[0-9]{4}.jpg')
+# Before splitting the quality setting off into a different named group
+# URL_PATH_PATTERN = re.compile(r'(?P<prefix>^/mcmags/[a-f0-9\-]*/[a-f0-9\-]*/(extralow|low|mid|high)/)[0-9]{4}.(bin|jpg)')
+# After splitting the quality setting off into a different named group
+URL_PATH_PATTERN = re.compile(r'(?P<prefix>^/mcmags/[a-f0-9\-]*/[a-f0-9\-]*)/(?P<imagequality>(extralow|low|mid|high)/)[0-9]{4}.(bin|jpg)')
 
 # Example URLs sampled 8 July 2022, deliberately using an advert from a magazine:
 # https://mcdatastore.blob.core.windows.net/mcmags/f3786b15-4b19-456e-9b58-2af137a35bcd/ba9c5bcb-cf96-4215-a2f5-841ddb4a119c/extralow/0046.jpg
@@ -70,6 +80,7 @@ URL_PATH_PATTERN = re.compile(r'(?P<prefix>^/mcmags/[a-f0-9\-/]*/mid/)[0-9]{4}.j
 # low:      362  x 512
 # mid:      905  x 1280
 # high:     1447 x 2048
+QUALITY_PATTERN = re.compile("(extralow|low|mid|high)")
 
 # Notes on the "high" image size with the "bin" file extension.
 # Running the Linux "file" command gives the output:
@@ -95,26 +106,51 @@ def main():
     pdf_fn, url = (opts[k] for k in ('<pdf>', '<url>'))
     url = urlparse(url)
     dpi = float(opts['--dpi'])
+    quality = str(opts['--quality'])
     print('URL is: {}'.format(url))
     print('File is: {}'.format(pdf_fn))
     print('DPI is {}'.format(dpi))
+    print('Quality is {}'.format(quality))
 
     m = URL_PATH_PATTERN.match(url.path)
     if not m:
         raise RuntimeError('URL path does not match expected pattern')
     prefix = m.group('prefix')
 
+    q = QUALITY_PATTERN.match(quality)
+    if not q:
+        raise RuntimeError("--quality= argument does not match any of the expected values: extralow|low|mid|high")
+
     c = canvas.Canvas(pdf_fn)
+    counter = 0
     with saving(c):
         for page_num in itertools.count(0):
             page_url = list(url)
-            page_url[2] = '{}{:04d}.jpg'.format(prefix, page_num)
+            file_extension = 'jpg'
+            if quality == 'high':
+                file_extension = 'bin'
+            page_url[2] = '{}/{}/{:04d}.{}'.format(prefix, quality, page_num, file_extension)
             page_url = urlunparse(page_url)
             print('Downloading page {} from {}...'.format(page_num, page_url))
 
             try:
                 with urlopen(page_url) as f:
-                    im = Image.open(f)
+                    # if: the extralow, low & mid quality "jpg" format URLs
+                    if quality == 'extralow' or quality == 'low' or quality == 'mid':
+                        im = Image.open(f)
+                    # else: the high quality "bin" format URL
+                    else:
+                        jpg_header = binascii.unhexlify('FFD8')
+                        filedata = f.read()[2:]
+                        imgdata = BytesIO(jpg_header + filedata)
+                        # with open('images/hightest.jpg', 'wb') as hightest:
+                        #     hightest.write(imgdata.getbuffer())
+                        try:
+                            im = Image.open(imgdata)
+                        except PIL.UnidentifiedImageError as uie:
+                            print("Page", page_num, "is not a valid image file. Unable to continue; exiting...")
+                            break
+
             except HTTPError as e:
                 if e.code == 404:
                     print('No image found => stopping')
@@ -127,6 +163,9 @@ def main():
             c.setPageSize((w*inch, h*inch))
             c.drawInlineImage(im, 0, 0, w*inch, h*inch)
             c.showPage()
+            counter += 1
+            if counter == 4:
+                break
 
 if __name__ == '__main__':
     main()
